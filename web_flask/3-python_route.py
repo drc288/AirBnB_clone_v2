56700d15f99a560fd85efdf2e7c4cb74d31586b8
David Rosero C <davidroserocalle@gmail.com>
2020-01-21 17:37:25 +0000

    add point 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,11 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def app_01():
     """ return the Hello HBNB!
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def app_02():
@@ -14,11 +16,13 @@ def app_02():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ return C plus the parameters
     """
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python', defaults={'text': "is fun"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -27,4 +31,6 @@ def python(text):
     """
     return "Python {}".format(text.replace("_", " "))
 
-app.run()
+
+if __name__ == "__main__":
+    app.run()
