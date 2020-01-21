9781f2a35d7fc6ee6134ff761de205bb2258f218
David Rosero C <davidroserocalle@gmail.com>
2020-01-21 17:19:22 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,8 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def web_flask():
+    """create a basic page
+    """
     return "Hello HBNB!"
 
-app.run()
+
+app.run(host="0.0.0.0")
