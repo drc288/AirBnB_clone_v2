f40c37d2534a0117e2148acba1d59239d4e8ec48
David Rosero C <davidroserocalle@gmail.com>
2020-01-21 17:34:30 +0000

    update point 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,16 +2,27 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def app_01():
+    """create basic page
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def app_02():
+    """create a secound page
+    """
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def app_03(text):
+    """create C is any case
+    """
     return "C {}".format(text.replace("_", " "))
 
-app.run()
+
+if __name__ == "__main__":
+    app.run()
