4f8b6f801619b0e57274ff30bac4f5a89a9bce80
David Rosero C <davidroserocalle@gmail.com>
2020-01-21 17:28:19 +0000

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,12 +2,20 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def app_01():
+    """create a basic page
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def app_02():
+    """create a secound page
+    """
     return "HBNB"
 
-app.run()
+
+if __name__ == "__main__":
+    app.run(debug=True)
