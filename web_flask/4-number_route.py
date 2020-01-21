539add65586d481ee9803326a11231bb5247ad4b
David Rosero C <davidroserocalle@gmail.com>
2020-01-21 17:45:43 +0000

    submit

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,11 +2,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
     """ return the Hello HBNB!
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -14,18 +16,21 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ return C plus the parameters
     """
     return "C {}".format(text.replace("_", " "))
 
-@app.route('/python', defaults={'text': "is fun"}, strict_slashes=False)
+
+@app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
     """ return Python
     """
     return "Python {}".format(text.replace("_", " "))
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
@@ -33,4 +38,6 @@ def number(n):
     """
     return "{} is a number".format(n)
 
-app.run(host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run()
